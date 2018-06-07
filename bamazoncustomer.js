df57fdbbd2b7d00e78fc1dc952be346b8faa6f19
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-07 17:17:13 -0500

    Playing around with cli-table

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,4 +1,18 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+var productList = [];
+var table = new Table({
+head:['Item ID', 'Item', 'Price', 'Qnty'],
+colWidths: [10, 40, 10 , 10]
+});
+
+// CLI Text colors I like to use 
+var FgBlue = "\x1b[34m";
+var FgWhite = "\x1b[0m";
+var FgCyan = "\x1b[36m";
+var FgGreen = "\x1b[32m";
+var FgMagenta = "\x1b[35m";
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -22,10 +36,24 @@ connection.connect(function(err) {
 
 function readProducts() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products order by product_name asc", function(err, res) {
       if (err) throw err;
       // Log all results of the SELECT statement
-      console.log(res);
+      for (var i = 0; i < res.length; i++) {
+        //console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name+ " | " + res[i].price + " | " + res[i].stock_quantity);
+        //debugger
+        productList.push(res);
+        //console.log (productList[i]);
+        table.push(
+            [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]
+        );
+        
+      }
       connection.end();
+      console.log(table.toString());
     });
   }
+
+// Prompt the user to provide location information.
+
+  
